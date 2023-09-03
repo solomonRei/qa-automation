4c43944df18438ba997071bd32d5140a9d0a9383
Eduard <dz6class@mail.ru>
2023-09-03 17:14:48 +0300

    Added new Runner, Updated tags in features

--- src/test/java/org/smeled/RunCucumberTests.java
+++ src/test/java/org/smeled/RunCucumberTests.java
@@ -1,22 +1,16 @@
 package org.smeled;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.IncludeTags;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("resources.features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.smeled")
-@IncludeTags("require_test")
-@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@require_test")
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "org.smeled",
+        plugin = {"pretty", "html:target/cucumber-reports"},
+        tags = "@Run"
+)
 public class RunCucumberTests {
 }
 
--- src/test/resources/features/bigshop.feature
+++ src/test/resources/features/bigshop.feature
@@ -1,32 +1,32 @@
 @bigshop
 Feature: BigShop Website Testing
 
-  @require_test
+  @Run
   Scenario: User navigates to the BigShop website
     Given the user opens the browser
     When the user navigates to "https://bigshop.md/"
     Then the website title should contain "BigShop"
 
-  @require_test
+  @Run
   Scenario: User searches for a product
     Given the user is on the BigShop homepage
     When the user searches for "smartphone"
     Then search results should be displayed
 
-  @require_test
+  @Run
   Scenario: User adds a product to the cart
     Given the user is on the product page
     When the user adds a laptop to the cart
     Then the cart should contain the added product
 
-  @optional_test
+  @Skip
   Scenario: User proceeds to checkout
     Given the user has products in the cart
     When the user goes to the cart
     And the user proceeds to checkout
     Then the user should see the checkout page
 
-  @undone_test
+  @Skip
   Scenario: User logs in to their account
     Given the user is on the login page
     When the user enters valid credentials
